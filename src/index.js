96ef177a3ef7f72de2d038f43414e08bf1a91e8e
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-03-28 15:38:33 +0530

    basic functionality implemented

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * `chain` is the instance of npm squel module
+ * `resource` includes the data to build the query
+ */
+module.exports = function(chain, resource) {
+	// iterate through each properties of `resource`
+	for (key in resource) {
+		if (!resource.hasOwnProperty(key)) {
+			continue;
+		}
+		switch (key) {
+			case 'fields':
+				resource[key].forEach(function(item) {
+					chain = chain.field(item);
+				})
+				break;
+
+			case 'where':
+				for (item in resource[key]) {
+					if (!resource[key].hasOwnProperty(item)) {
+						continue;
+					}
+					chain = chain.where(item + '=' + resource[key][item])
+				}
+				break;
+
+			case 'set':
+				for (item in resource[key]) {
+					if (!resource[key].hasOwnProperty(item)) {
+						continue;
+					}
+					chain = chain.set(item, resource[key][item])
+				}
+				break;
+		}
+	}
+}
