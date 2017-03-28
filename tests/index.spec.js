8c5737700db8dfa1af5e8519ca6f090057d42f96
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-03-28 17:34:57 +0530

    better tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,16 +1,36 @@
-const makeQuery = require('./../src');
-const sql = require('squel');
+/*global it describe*/
 
-let resource = {
-	fields: ['name', 'age', 'address'],
-	where: {
-		name: 'Divya',
-		age: '44'
-	}
-}
+var makeQuery = require('./../src');
+var sql = require('squel');
+var expect = require('chai').expect;
 
-let chain = sql.select().from('users');
+describe('Testing sqlify', function() {
+	it('should make good query (type 1: SELECT)', function(done) {
 
-makeQuery(chain, resource);
+		var resource = {
+			fields: ['name', 'age', 'address'],
+			where: {
+				name: 'Divya',
+				age: '44'
+			}
+		};
+		var chain = sql.select().from('users');
+		makeQuery(chain, resource);
+		expect(chain.toString()).to.equal('SELECT name, age, address FROM users WHERE (name=Divya) AND (age=44)');
+		done();
+	});
 
-console.log(chain.toString());
+	it('should make good query (type 2: INSERT)', function(done) {
+
+		var resource = {
+			set: {
+				name: 'Divya',
+				age: 44
+			}
+		};
+		var chain = sql.insert().into('users');
+		makeQuery(chain, resource);
+		expect(chain.toString()).to.equal('INSERT INTO users (name, age) VALUES (\'Divya\', 44)');
+		done();
+	});
+});
