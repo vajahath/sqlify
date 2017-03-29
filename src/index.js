a6c2e312a8bc1e5e773330d91a9f3c5bf1845b77
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-03-29 12:42:02 +0530

    linting the hell out

--- src/index.js
+++ src/index.js
@@ -14,42 +14,42 @@ module.exports = function(chain, resource) {
 			continue;
 		}
 		switch (key) {
-			case 'fields':
-				resource[key].forEach(function(item) {
-					chain = chain.field(item);
-				});
-				break;
+		case 'fields':
+			resource[key].forEach(function(item) {
+				chain = chain.field(item);
+			});
+			break;
 
-			case 'where':
-				for (item in resource[key]) {
-					if (!resource[key].hasOwnProperty(item)) {
-						continue;
-					}
+		case 'where':
+			for (item in resource[key]) {
+				if (!resource[key].hasOwnProperty(item)) {
+					continue;
+				}
 
-					appendingValue = resource[key][item];
+				appendingValue = resource[key][item];
 					// modify appendingValue to include 's if necessary
-					switch (typeof(resource[key][item])) {
-						case 'numberd':
-							break;
-						case 'stringd':
-							appendingValue = '\'' + appendingValue + '\'';
-							break;
-						default:
-							lme.e('SQLIFY ERR: a type other than "string" or "number" encountered in \'where\'');
-							throw new Error('a type other than "string" or "number" encountered');
-					}
-					chain = chain.where(item + '=' + appendingValue);
+				switch (typeof(resource[key][item])) {
+				case 'numberd':
+					break;
+				case 'stringd':
+					appendingValue = '\'' + appendingValue + '\'';
+					break;
+				default:
+					lme.e('SQLIFY ERR: a type other than "string" or "number" encountered in \'where\'');
+					throw new Error('a type other than "string" or "number" encountered');
 				}
-				break;
+				chain = chain.where(item + '=' + appendingValue);
+			}
+			break;
 
-			case 'set':
-				for (var item in resource[key]) {
-					if (!resource[key].hasOwnProperty(item)) {
-						continue;
-					}
-					chain = chain.set(item, resource[key][item]);
+		case 'set':
+			for (var item in resource[key]) {
+				if (!resource[key].hasOwnProperty(item)) {
+					continue;
 				}
-				break;
+				chain = chain.set(item, resource[key][item]);
+			}
+			break;
 		}
 	}
 };
