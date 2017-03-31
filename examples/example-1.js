bd68dddd672692a9cd6c2d3dfb23271eb278b6d1
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-03-31 10:38:36 +0530

    linting the hell out

--- examples/example-1.js
+++ examples/example-1.js
@@ -4,22 +4,22 @@ var lme = require('lme');
 
 var resource = {
 	fields: [
-		"service_types.service_title",
-		"service_pricing.service_pricing_title",
-		"service_pricing_sub.service_pricing_sub_title",
-		"service_pricing_cost.service_pricing_cost",
-		"service_pricing_cost.service_pricing_currency"
+		'service_types.service_title',
+		'service_pricing.service_pricing_title',
+		'service_pricing_sub.service_pricing_sub_title',
+		'service_pricing_cost.service_pricing_cost',
+		'service_pricing_cost.service_pricing_currency'
 	],
 	join: [
-		["service_pricing", null, "service_types.service_id = service_pricing.service_type"],
-		["service_pricing_sub", null, "service_pricing.service_pricing_id = service_pricing_sub.service_pricing_id"],
-		["service_pricing_cost", null, "service_pricing_sub.service_pricing_sub_id = service_pricing_cost.service_pricing_sub_id"]
+		['service_pricing', null, 'service_types.service_id = service_pricing.service_type'],
+		['service_pricing_sub', null, 'service_pricing.service_pricing_id = service_pricing_sub.service_pricing_id'],
+		['service_pricing_cost', null, 'service_pricing_sub.service_pricing_sub_id = service_pricing_cost.service_pricing_sub_id']
 	],
 	where: {
-		"service_types.service_id": "something",
-		"service_pricing_cost.service_pricing_currency": 'something else'
+		'service_types.service_id': 'something',
+		'service_pricing_cost.service_pricing_currency': 'something else'
 	}
-}
+};
 var chain = sql.select({ separator: '\n' }).from('service_types');
 makeQuery(chain, resource);
 
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,7 +3,7 @@
 var makeQuery = require('./../src');
 var sql = require('squel');
 var expect = require('chai').expect;
-var lme = require('lme');
+// var lme = require('lme');
 
 describe('Testing sqlify', function() {
 	it('should make good query (type 2: INSERT)', function(done) {
@@ -26,22 +26,22 @@ describe('Testing sqlify', function() {
 		it('case 1 (SELECT fields FROM table JOIN JOIN JOIN WHERE cond.)', function(done) {
 			var resource = {
 				fields: [
-					"service_types.service_title",
-					"service_pricing.service_pricing_title",
-					"service_pricing_sub.service_pricing_sub_title",
-					"service_pricing_cost.service_pricing_cost",
-					"service_pricing_cost.service_pricing_currency"
+					'service_types.service_title',
+					'service_pricing.service_pricing_title',
+					'service_pricing_sub.service_pricing_sub_title',
+					'service_pricing_cost.service_pricing_cost',
+					'service_pricing_cost.service_pricing_currency'
 				],
 				join: [
-					["service_pricing", null, "service_types.service_id = service_pricing.service_type"],
-					["service_pricing_sub", null, "service_pricing.service_pricing_id = service_pricing_sub.service_pricing_id"],
-					["service_pricing_cost", null, "service_pricing_sub.service_pricing_sub_id = service_pricing_cost.service_pricing_sub_id"]
+					['service_pricing', null, 'service_types.service_id = service_pricing.service_type'],
+					['service_pricing_sub', null, 'service_pricing.service_pricing_id = service_pricing_sub.service_pricing_id'],
+					['service_pricing_cost', null, 'service_pricing_sub.service_pricing_sub_id = service_pricing_cost.service_pricing_sub_id']
 				],
 				where: {
-					"service_types.service_id": "something",
-					"service_pricing_cost.service_pricing_currency": 'something else'
+					'service_types.service_id': 'something',
+					'service_pricing_cost.service_pricing_currency': 'something else'
 				}
-			}
+			};
 			var chain = sql.select().from('users');
 			makeQuery(chain, resource);
 
@@ -52,13 +52,13 @@ describe('Testing sqlify', function() {
 		it('case 2 (SELECT fields FROM table WHERE cond.)', function(done) {
 			var resource = {
 				fields: [
-					"fabric_id",
-					"fabric_title"
+					'fabric_id',
+					'fabric_title'
 				],
 				where: {
-					"fabrics.fabric_type": 'something'
+					'fabrics.fabric_type': 'something'
 				}
-			}
+			};
 			var chain = sql.select().from('fabrics');
 			makeQuery(chain, resource);
 
@@ -67,5 +67,5 @@ describe('Testing sqlify', function() {
 			done();
 		});
 
-	})
+	});
 });
