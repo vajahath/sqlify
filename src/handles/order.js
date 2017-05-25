2c6e84d5e139c0745792833ba4e44242e47ae977
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-05-25 16:58:01 +0530

    squel reference correction

--- src/handles/order.js
+++ src/handles/order.js
@@ -1,6 +1,6 @@
 /**
  * implementation of:
- * https://hiddentao.com/squel/api.html#select_field
+ * https://hiddentao.com/squel/api.html#select_order
  */
 
 module.exports = function(chain, resource) {
