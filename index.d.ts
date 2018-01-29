b81b8219d169f055633e81089cdd8335de3fccb6
Vajahath Ahmed <vajuoff.1@gmail.com>
2018-01-29 18:33:30 +0530

    better confs

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * This is the definition file for your package
- * ============================================
- * Change this file as per Typescript's guide to
- * write high quality definition file.
- * Link: https://www.typescriptlang.org/docs/handbook/declaration-files/introduction.html\
- *
- */
-declare module "sqlify ts";
