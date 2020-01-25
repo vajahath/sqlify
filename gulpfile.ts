899505e3d9b62387bdb391198df8931e262613c4
Vajahath Ahmed <vajuoff.1@gmail.com>
2020-01-25 13:34:00 +0530

    security(dev): update packages to address security warnings

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,11 +1,13 @@
 import * as del from 'del';
 import * as gulp from 'gulp';
 
-// copy assets files from src to dist
-gulp.task('copy-assets', () => {
-	return gulp.src(['src/**/*', '!src/**/*.ts']).pipe(gulp.dest('dist/'));
-});
+async function cleanBuild() {
+	return await del(['dist/**/*', 'dist/**/.*']);
+}
 
-gulp.task('clean-build', () => {
-	return del.sync(['dist/**/*', 'dist/**/.*']);
-});
+function copyAssets() {
+	return gulp.src(['src/**/*', '!src/**/*.ts']).pipe(gulp.dest('dist/'));
+}
+
+exports['clean-build'] = cleanBuild;
+exports['copy-assets'] = copyAssets;
